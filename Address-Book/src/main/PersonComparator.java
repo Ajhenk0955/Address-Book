58cea109ee5e679c5aeaefed47b025bc5d561065
iagainey <iagainey7197@eagle.fgcu.edu>
2016-03-04 09:09:52 -0500

    Added Filters
    Added Last name, first name, state, city, zip, and phone filters.
    
    Place -1 when not equal, possible change to 1 could be needed.

--- Address-Book/src/main/PersonComparator.java
+++ Address-Book/src/main/PersonComparator.java
@@ -6,7 +6,12 @@ import java.util.Comparator;
  * this enum is used to compare entries<Person> 
  */
 enum PersonComparator implements Comparator<Person> {
-
+	/*
+	 * Returns
+	 * -1 o1 < o2
+	 *  0 o1 == o2
+	 *  1 o1 > 02
+	*/
 	ZIP_SORT {
 		public int compare(Person o1, Person o2) {
 			return Integer.valueOf(o1.getZip()).compareTo(Integer.valueOf(o2.getZip()));
@@ -20,6 +25,36 @@ enum PersonComparator implements Comparator<Person> {
 	LAST_NAME_SORT {
 		public int compare(Person o1, Person o2) {
 			return o1.getLastName().compareTo(o2.getLastName());
+		}
+	},
+	LAST_NAME_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getLastName().equals(o2.getLastName()))? 0 : -1; // might have to change -1 to 1
+		}
+	},
+	FIRST_NAME_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getFirstName().equals(o2.getFirstName()))? 0 : -1; // might have to change -1 to 1
+		}
+	},
+	ZIP_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getZip().equals(o2.getZip()))? 0 : -1; // might have to change -1 to 1
+		}
+	},
+	STATE_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getState().equals(o2.getState()))? 0 : -1; // might have to change -1 to 1
+		}
+	},
+	CITY_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getCity().equals(o2.getCity()))? 0 : -1; // might have to change -1 to 1
+		}
+	},
+	PHONE_FILTER{
+		public int compare(Person o1, Person o2)	{
+			return (o1.getPhone().equals(o2.getPhone()))? 0 : -1; // might have to change -1 to 1
 		}
 	};
 
